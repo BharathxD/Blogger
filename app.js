419fce3eea89354aab918d0b045a5f3d23433014
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-07-27 18:35:49 +0530

    Fixed Host

--- app.js
+++ app.js
@@ -283,6 +283,6 @@ app.post("/report", function (req, res) {
   );
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
