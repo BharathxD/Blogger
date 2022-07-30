04286270e1b7a6fb244c90b3d353a1930fa9651c
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-07-30 23:23:12 +0530

    misc

--- public/js/index.js
+++ public/js/index.js
@@ -23,3 +23,11 @@ const swup = new Swup();
     focusableElementsOffsetX: 4,
     focusableElementsOffsetY: 4,
   });
+  tinymce.init({
+    selector: 'textarea',
+    plugins: 'a11ychecker advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinycomments tinymcespellchecker',
+    toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter image editimage pageembed permanentpen table tableofcontents',
+    toolbar_mode: 'floating',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+  });
