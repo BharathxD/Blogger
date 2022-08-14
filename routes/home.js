512583bb7f532fd45632efecbfa18fc2dcba8fa3
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-08-14 22:27:22 +0530

    Async

--- routes/home.js
+++ routes/home.js
@@ -1,68 +1,45 @@
 const express = require("express");
 const router = express.Router();
 
-const axios = require("axios");
-const https = require("https");
-
-const Post = require("../models/post_model");
-
-/* Initializing Global Variables */
-
-let lat,
-  lon,
+var Lat,
+  Lon,
   cityName,
   weatherData,
   weatherTemp,
   weatherMain,
   weatherIcon,
   weatherDescription,
-  weatherStatus;
+  weatherStatus = true;
+  
 
-weatherStatus = true;
+const axios = require("axios").default;
+const https = require("https");
 
-/* ------------------ */
-/* IP Geolocation API */
+const Post = require("../models/post_model");
 
-axios
-    .get(
-      //! Requires Ipgeolocation API 
-      `https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.IPG_API_KEY}`
-    )
-    .then( async (response) => {
-      const responseData = await response.data;
-      lat = responseData.latitude;
-      lon = responseData.longitude;
-      console.log(lat);
-    })
-    .catch((error) => {
-      console.log(error);
+
+
+router.get("/", async (req, res) => {
+
+   await axios
+  .get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.IPG_API_KEY}`)
+  .then((response) => {
+    Lat = response.data.latitude;
+    Lon = response.data.longitude;
+  })
+  .catch((error) => {console.log(error);});
+
+const url = `https://api.openweathermap.org/data/2.5/weather?&lat=${Lat}&lon=${Lon}&appid=${process.env.Weather_API_KEY}&units=metric`;
+ https.get(url, (response) => {
+    response.on("data", async (data) => {
+      weatherData = await JSON.parse(data);
+      cityName = await weatherData.name;
+      weatherTemp = await weatherData.main.temp;
+      weatherMain = await weatherData.weather[1].main;
+      weatherDescription = await weatherData.weather[0].description;
+      weatherIcon = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
     });
-
-/* Home Route */
-
-router.get("/", (req, res) => {
-
-  /* Weather API */
-
-  const apikey = process.env.Weather_API_KEY;
-  const unit = "metric";
-  //! Requires OpenweatherAPI 
-  const url = `https://api.openweathermap.org/data/2.5/weather?&appid=${apikey}&lat=${lat}&lon=${lon}&units=${unit}`;
-  https.get(url, (response) => {
-    if (response.statusCode === 404) {
-      weatherStatus = false;
-    } else {
-      response.on("data", (data) => {
-        weatherData = JSON.parse(data);
-        console.log(weatherData);
-        cityName = weatherData.name;
-        weatherTemp = weatherData.main.temp;
-        weatherMain = weatherData.weather[1].main;
-        weatherDescription = weatherData.weather[0].description;
-        weatherIcon = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-      });
-    }
-  });
+});
 
   /* Incorporating all the posts from PostDB into the homescreen at once */
 
