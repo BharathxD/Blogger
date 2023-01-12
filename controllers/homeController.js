d6faf21206bae6c55dd5fbd3811b7f8f65880306
BharathXD <BharathxxD@gmail.com>
2023-01-12 19:21:31 +0530

    Refactored and Debugged the Code

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -23,58 +23,42 @@ const https = require("https");
 const Post = require("../models/post_model");
 
 const homeController = async (req, res) => {
-
+  try {
     //* IP Geolocation API 
     //! Requires API Key
-  
-    await axios
-      .get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.IPG_API_KEY}`)
-      .then((response) => {
-        Lat = response.data.latitude;
-        Lon = response.data.longitude;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    const { data: locationData } = await axios.get(
+      `https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.IPG_API_KEY}`
+    );
+    const { latitude: Lat, longitude: Lon } = locationData;
+
     //* openWeather API 
     //! Requires API Key
-    
-    const url = `https://api.openweathermap.org/data/2.5/weather?&lat=${Lat}&lon=${Lon}&appid=${process.env.Weather_API_KEY}&units=metric`;
-    https
-      .get(url, (res) => {
-        let data = "";
-        res.on(data, (chunk) => {
-          data += chunk;
-        });
-        res.on("end", async () => {
-          weatherData = await JSON.parse(data);
-          weatherStatus = weatherData.cod == 200 ? true : false;
-          cityName = weatherData.name;
-          weatherTemp = weatherData.main.temp;
-          weatherMain = weatherData.weather[0].main;
-          weatherDescription = weatherData.weather[0].description;
-          weatherIcon = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-        });
-      })
-      .on("error", (err) => {
-        console.log(err.message);
-      });
-  
+    const { data: weatherData } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?&lat=${Lat}&lon=${Lon}&appid=${process.env.Weather_API_KEY}&units=metric`
+    );
+
+    const { name: cityName, main, weather } = weatherData;
+    const [{ description, icon, main: weatherMain }] = weather;
+    const weatherTemp = main.temp;
+    const weatherIcon = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    const weatherDescription = description;
+    const weatherStatus = weatherData.cod === 200;
+
     /* Incorporating all the posts from PostDB into the homescreen at once */
-  
-    Post.find((err, foundItems) => {
-      !err
-        ? res.render("home", {
-            posts: foundItems,
-            cityName: cityName,
-            weatherTemp: weatherTemp,
-            weatherIcon: weatherIcon,
-            weatherDescription: weatherDescription,
-            weatherMain: weatherMain,
-            weatherStatus: weatherStatus,
-          })
-        : console.log(err);
+    const foundItems = await Post.find();
+    res.render("home", {
+      posts: foundItems,
+      cityName,
+      weatherTemp,
+      weatherIcon,
+      weatherDescription,
+      weatherMain,
+      weatherStatus,
     });
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 module.exports = homeController;
